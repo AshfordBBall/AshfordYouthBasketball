58a91cbdfdfe66671ab3e49758c9bddee1d62a15
AshfordBBall <tafflethenarwhal@gmail.com>
2025-02-21 22:52:55 +0000

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Add to end of body
+<script>
+    // Smooth scroll for navigation
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+</script>
